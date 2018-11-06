a563a89cdf1739890368003a30d51d881baeaaa5
emerson <emerson.r.alves.a@gmail.com>
2018-11-06 21:46:34 -0200

    Added Routes and new Components [Body, NotFound, MyCourses and Sub-banner]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {AppComponent} from 'src/app/app.component';
+import {MycoursesComponent} from 'src/app/common/components/mycourses/mycourses.component';
+import {NotFoundComponent} from 'src/app/common/components/not-found/not-found.component';
+import {BodyComponent} from 'src/app/common/components/body/body.component';
+
+const routes: Routes = [
+  {path: '', component: BodyComponent},
+  {path: 'my-courses', component: MycoursesComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
