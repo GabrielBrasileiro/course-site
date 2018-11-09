9e1b3822ad1e7cd0bd976406be16ad19703cc681
Gabriel <gah.lucas@gmail.com>
2018-11-08 22:53:12 -0300

    Addition of firebase in login component
    Signed-off-by: Gabriel <gah.lucas@gmail.com>

--- src/app/common/dialogs/login-dialog/login-dialog.component.ts
+++ src/app/common/dialogs/login-dialog/login-dialog.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AuthService } from '../../services/auth/auth.service'
 
 @Component({
   selector: 'app-login-dialog',
   templateUrl: './login-dialog.component.html',
   styleUrls: ['./login-dialog.component.css']
 })
-export class LoginDialogComponent implements OnInit {
+export class LoginDialogComponent {
 
-  constructor() { }
+  constructor(public auth: AuthService, private router: Router) { }
 
-  ngOnInit() {
+  async signInWithGoogle() {
+    await this.auth.googleLogin();
+    return await this.afterSignIn();
+  }
+
+  private afterSignIn() {
+    return this.router.navigate(['/']);
   }
 
 }
