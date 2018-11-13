51cbe64bc188bde4faed4ec4cff4f7ab9c8fdffa
emerson <emerson.r.alves.a@gmail.com>
2018-11-12 22:59:15 -0200

    Implementation of user-option-dialog

---
+++ src/app/common/components/user-option-dialog/user-option-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-option-dialog',
+  templateUrl: './user-option-dialog.component.html',
+  styleUrls: ['./user-option-dialog.component.css']
+})
+export class UserOptionDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
